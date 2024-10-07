98df1daeb7c8e6bae23b7e3d7cfc8c088d11284e
Murilo <murilorodriguesrrs@gmail.com>
2024-10-07 15:36:56 -0300

    test: Teste de compilacao nos .c

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include "../include/say_hello.h"
+#include <stdio.h>
+
+int main() {
+
+  sayHello();
+
+  printf("Hello, World!\n");
+
+  return 0;
+}
---
+++ src/say_hello.c
@@ -0,0 +1,4 @@
+#include "../include/say_hello.h"
+#include <stdio.h>
+
+void sayHello() { printf("Hello, World 2!\n"); }
