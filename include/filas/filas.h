3332a18c341388161012633e0276ca8df164aa8d
Murilo <murilorodriguesrrs@gmail.com>
2024-10-16 02:32:35 -0300

    feat: filas

---
+++ include/filas/filas.h
@@ -0,0 +1,22 @@
+#include "../usuario/usuario.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No {
+  Usuario dado;       // Dado armazenado no no
+  struct No *proximo; // Ponteiro para o próximo no
+} No;
+
+typedef struct Fila {
+  No *frente; // Ponteiro para o primeiro elemento da fila
+  No *tras;   // Ponteiro para o último elemento da fila
+} Fila;
+
+Fila *criarFila(); // Cria uma nova fila
+
+void addFila(Fila *fila, int dado); // Adiciona um dado à fila
+
+Usuario rmFila(Fila *fila); // Remove e retorna o dado do início da fila
+
+bool filaVazia(Fila *fila); // Verifica se a fila está vazia
