9fe8097ec14483f5ef68c1c99363e059764194eb
Murilo <murilorodriguesrrs@gmail.com>
2024-10-29 02:15:07 -0300

    fix: comentando pro resto do codigo rodar

--- src/ingredientes/ingredientes.c
+++ src/ingredientes/ingredientes.c
@@ -1,17 +1,17 @@
 #include "ingredientes.h"
 #include <stdio.h>
 
-// Funcao para definir ingrediente
-DEFINIR_INGREDIENTES()
+// // Funcao para definir ingrediente
+// DEFINIR_INGREDIENTES()
 
-// Funcao para definir aceitacao dos ingredientes
-DEFINIR_ACEITACAO()
+// // Funcao para definir aceitacao dos ingredientes
+// DEFINIR_ACEITACAO()
 
-// Funcao para definir quantidades minima e maxima de ingredientes
-DEFINIR_QTD_ING()
+// // Funcao para definir quantidades minima e maxima de ingredientes
+// DEFINIR_QTD_ING()
 
-// Funcao para definir tempo de reposicao de ingrediente
-DEFINIR_TEMPO_SUBSTITUICAO()
+// // Funcao para definir tempo de reposicao de ingrediente
+// DEFINIR_TEMPO_SUBSTITUICAO()
 
-// Funcao para definir minimo de substituicao
-DEFINIR_MINIMO_SUBSTITUICAO()
+// // Funcao para definir minimo de substituicao
+// DEFINIR_MINIMO_SUBSTITUICAO()
