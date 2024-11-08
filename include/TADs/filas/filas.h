5b1138b7592ea6992a0d4ac995952b8ff89f65ff
Murilo <murilorodriguesrrs@gmail.com>
2024-11-08 01:45:41 -0300

    fix: Retornando usuario como ponteiro

--- include/TADs/filas/filas.h
+++ include/TADs/filas/filas.h
@@ -1,25 +1,30 @@
+#ifndef FILAS_H
+#define FILAS_H
+
 #include "../usuarios/usuarios.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct No {
-  Usuario dado;       // Dado armazenado no no
+  Usuario *dado;      // Dado armazenado no no
   struct No *proximo; // Ponteiro para o próximo no
 } No;
 
-typedef struct Fila {
+typedef struct {
   No *frente;
   No *tras;
   int id;
 } Fila;
 
-Fila *criarFila();
+Fila *criarFila(int id);
 
 void addFila(Fila *fila, Usuario *dado);
 
-Usuario rmFila(Fila *fila); // Remove e retorna o dado do início da fila
+Usuario *rmFila(Fila *fila); // Remove e retorna o dado do início da fila
 
 void exibeFila(Fila *fila);
 
 bool filaVazia(Fila *fila);
+
+#endif // FILAS_H
