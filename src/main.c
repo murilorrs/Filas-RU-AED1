c21fec1cd5b6fcfc64c976f47c2febf2425f6493
Murilo <murilorodriguesrrs@gmail.com>
2024-12-06 02:48:00 -0300

    feat: Utilização menu

--- src/main.c
+++ src/main.c
@@ -1,10 +1,53 @@
 #include "../include/App/app.h"
 
 int main() {
+  int choice;
+  int param1, param2;
+  system("clear");
 
-  realizarSimulacao(150, 1);
-  realizarSimulacao(210, 2);
-  realizarSimulacao(150, 3);
+  while (1) {
+    printf("\033[1;34m========================================\033[0m\n");
+    printf("\033[1;36m        SIMULADOR DO RESTAURANTE \033[0m\n");
+    printf("\033[1;34m========================================\033[0m\n\n");
+
+    printf("\033[1;33mMenu:\033[0m\n");
+    printf("\033[1;32m1.\033[0m Executar simulacao 1 - Cafe da Manha\n");
+    printf("\033[1;32m2.\033[0m Executar simulacao 2 - Almoco\n");
+    printf("\033[1;32m3.\033[0m Executar simulacao 3 - Jantar\n");
+    printf("\033[1;35m4.\033[0m Executar todas as simulacoes\n");
+    printf("\033[1;31m0.\033[0m Sair\n");
+
+    printf("\n\033[1;37m=> \033[0m");
+    scanf("%d", &choice);
+
+    switch (choice) {
+    case 1:
+      realizarSimulacao(10, 1);
+      break;
+    case 2:
+      realizarSimulacao(10, 2);
+      break;
+    case 3:
+      realizarSimulacao(150, 3);
+      break;
+    case 4:
+      realizarSimulacao(10, 1);
+      realizarSimulacao(10, 2);
+      realizarSimulacao(150, 3);
+      break;
+    case 0:
+      printf("\033[1;31mSaindo da simulacao.\033[0m\n");
+      return 0;
+    default:
+      printf("\033[1;31mOpcao invalida. Tente novamente! \033[0m\n");
+    }
+
+    printf("\n\033[1;34m========================================\033[0m\n");
+    printf("\033[1;37mPressione Enter para continuar...\033[0m\n");
+    getchar();
+    getchar();
+    system("clear");
+  }
 
   return 0;
 }
